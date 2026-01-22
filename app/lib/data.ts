5306aa652b1835b0d40e5c9e889d9188a33f95ee
Pasala Bala Arun <108626342+arunpasala@users.noreply.github.com>
2026-01-22 15:13:55 -0500

    static and dynamic rendering

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,7 +17,13 @@ const sql = postgres(process.env.POSTGRES_URL_NON_POOLING!, {
 
 export async function fetchRevenue() {
   try {
+
+    //artificiallu delay a response
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    // console.log('Data fetch completed after 3 seconds.');
     return data;
   } catch (error) {
     console.error("Database Error:", error);
